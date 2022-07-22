75791fa8d4a6f95249bbba4c6f43d3b2656a1da3
anshul <anshul_mittal12@hotmail.com>
2022-07-22 17:45:35 +0530

    add: dropdown without api

--- src/components/shared/SearchBox.js
+++ src/components/shared/SearchBox.js
@@ -1,9 +1,8 @@
-import Link from 'next/link';
-import renderHTML from 'react-render-html';
 import { useState, useEffect } from 'react';
 import { listSearch } from '../../actions/blog';
-
-const SearchBox = ({hideBanner}) => {
+import Async from 'react-select/async';
+import Router from 'next/router';
+const SearchBox = ({ hideBanner }) => {
     const [values, setValues] = useState({
         city: undefined,
         search: undefined,
@@ -11,17 +10,18 @@ const SearchBox = ({hideBanner}) => {
         searched: false,
         message: ''
     });
-
+    const [query, setQuery] = useState('');
+    const[selectedOption,setSelectedOption] = useState('');
     const { city, search, results, searched, message } = values;
 
     const searchSubmit = e => {
         e.preventDefault();
         listSearch({ search }).then(data => {
-            if(data) {
+            if (data) {
                 setValues({ ...values, results: data, searched: true, message: `${data.length} blogs found` });
             }
         })
-        .catch(err => console.log(err));
+            .catch(err => console.log(err));
     };
 
     const handleChange = name => e => {
@@ -34,19 +34,40 @@ const SearchBox = ({hideBanner}) => {
             <div className="jumbotron bg-white">
                 {message && <p className="pt-4 text-muted font-italic">{message}</p>}
                 <datalist id="datalistOptions">
-                {results.map((blog, i) => {
-                    return (
-                        <option key={i} value={blog.title}>
-                        {blog.title}
-                        </option>                        
-                    );
-                })}
+                    {results.map((blog, i) => {
+                        return (
+                            <option key={i} value={blog.title}>
+                                {blog.title}
+                            </option>
+                        );
+                    })}
                 </datalist>
-                    
+
             </div>
         );
     };
+    // api to be changed , debounce to be added
+    const loadOptions = async () => {
+        const res = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+        const data = await res.json();
+        if (data.meals) {
+            return data.meals
+        }
+        return []
 
+    };
+
+    const onDropdownChange=(item)=>{
+        setSelectedOption(item);
+        Router.push('/property/'+(item.name||'shipra-apartments'))
+    }
+    const SearchIcon = props => {
+        return (
+            <span className="material-symbols-outlined mx-2">
+                search
+            </span>
+        );
+    };
     const searchForm = () => (
         <form onSubmit={searchSubmit} className="mx-auto">
             <div className="row">
@@ -59,22 +80,23 @@ const SearchBox = ({hideBanner}) => {
                         <option value="Other">Other</option>
                     </select>
                 </div>
-                <div className="input col-md-7">
-                    <input type='text' name='search' className="form-control bdr-r-0 h-44" onChange={handleChange('search')} list="datalistOptions" id="exampleDataList" placeholder="Type your property name..." />
-                    <datalist id="datalistOptions">
-                    {results.map((blog, i) => {
-                        return (
-                            <option key={i} value={blog.title}>
-                            {blog.title}
-                            </option>                        
-                        );
-                    })}
-                    </datalist>
-                    </div>
+                <div className="col-md-7 p-1">
+                    <Async noOptionsMessage={() => 'There are no such properties yet'}
+                        onInputChange={(value) => setQuery(value)}
+                        loadOptions={loadOptions}
+                        getOptionLabel={(e) => e.strMeal}
+                        getOptionValue={(e) => e.strMeal}
+                        components={{ DropdownIndicator: SearchIcon }}
+                        value={selectedOption}
+                        onChange={(selectedItem)=>{onDropdownChange(selectedItem)}}
+                        instanceId="unique"
+                        placeholder="Search the localities/builder or Apartments"
+                    />
+                </div>
 
                 <div className="button col-md-2">
                     <button className="btn btn-block text-white bdr-r-0 h-44" type="submit">
-                    <i className="fa-solid fa-magnifying-glass"></i>
+                        <i className="fa-solid fa-magnifying-glass"></i>
                         Search
                     </button>
                 </div>
@@ -84,8 +106,11 @@ const SearchBox = ({hideBanner}) => {
 
     return (
         <div className="container-fluid">
-            {!hideBanner && <h2 className='title_color text-center mt-3'>India's largest reviews site</h2> }
-            <div className="pt-3 pb-5 search-box mx-auto">{searchForm()}</div>            
+            {!hideBanner && <h2 className='title_color text-center mt-3'>India's largest reviews site</h2>}
+            <div className="pt-3 pb-5 search-box mx-auto">{searchForm()}</div>
+            <style>{`.try{
+                height:100px;
+            }`}</style>
         </div>
     );
 };
